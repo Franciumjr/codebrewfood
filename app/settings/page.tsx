346db162e86825339cb57577f123665961cdbf85
jfkul <joshua.kulik@icloud.com>
2026-04-10 19:15:21 +1000

    Add log in button without functionality

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -6,7 +6,14 @@ import { ChevronLeft } from 'lucide-react'
 const page = () => {
   return (
     <div>
+      <div>
         <Link href="/"><ChevronLeft></ChevronLeft></Link>
+      </div>
+      <div className="flex items-center justify-center h-screen">
+        <Button variant="outline" size="lg" className="content-center">
+          Sign up/log in with email
+        </Button>
+      </div>
     </div>
   )
 }
